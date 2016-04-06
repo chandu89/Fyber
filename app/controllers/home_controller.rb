9a318000781eee21976a292733d7ed1dbc2bf339
chandu89 <chandanchoubey@india.cogbooks.com>
2016-04-06 17:24:09 +0530

    Changing root and added link to show page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -12,7 +12,6 @@ class HomeController < ApplicationController
 		hashkey= Digest::SHA1.hexdigest "appid=157&apple_idfa=E7CE4B3-F68A-44D9-A923-F4E48D92B31E&apple_idfa_tracking_enabled=true&device_id=2b6f0cc904d137be2e1730235f5664094b83&ip=109.235.143.113&locale=de&offer_types=112&os_version=6.0&page=#{page}&pub0=#{pub}&timestamp=#{current_time}&uid=#{uid}&b07a12df7d52e6c118e5d47d3f9e60135b109a1f"
 		response = HTTParty.get("http://api.fyber.com/feed/v1/offers.json?appid=157&apple_idfa=E7CE4B3-F68A-44D9-A923-F4E48D92B31E&apple_idfa_tracking_enabled=true&device_id=2b6f0cc904d137be2e1730235f5664094b83&ip=109.235.143.113&locale=de&offer_types=112&os_version=6.0&page=#{page}&pub0=#{pub}&timestamp=#{current_time}&uid=#{uid}&hashkey=#{hashkey}")
 		@result = response.parsed_response["offers"]
-		Rails.logger.info("--#{@result.inspect}--")
 	end
 	private
 
